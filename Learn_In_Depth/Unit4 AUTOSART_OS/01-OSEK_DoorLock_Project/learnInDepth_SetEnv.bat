c8198271d7b697268f3280b6beb38d1cb4dd1356
osama <usamamahmoud.2022@gmail.com>
2024-05-17 16:24:06 +0300

    add DoorLock indication project using OSEK

---
+++ Learn_In_Depth/Unit4 AUTOSART_OS/01-OSEK_DoorLock_Project/learnInDepth_SetEnv.bat
@@ -0,0 +1,8 @@
+@echo off
+SET ARM_CROSS_TOOLCHAIN_PATH=G:\OSAMAA\Embedded System\Learn In Depth\AUTOSAR IN DEPTH\Unit4  AUTOSAR OS\ARM_Toolchain\ARM\bin
+SET PYTHON_PATH=C:\Users\Osama\AppData\Local\Programs\Python\Python38\
+SET goil_PATH=..\..\
+
+SET PATH=%ARM_CROSS_TOOLCHAIN_PATH%;%PYTHON_PATH%;%goil_PATH%;%PATH%
+
+echo Environment variables have been set.
