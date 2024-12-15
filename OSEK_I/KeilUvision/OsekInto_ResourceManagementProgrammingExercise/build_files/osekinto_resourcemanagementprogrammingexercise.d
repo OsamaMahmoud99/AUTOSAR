0304b06a6e53695e48298889befb7811d62ac222
osama <usamamahmoud.2022@gmail.com>
2024-12-15 19:12:00 +0200

    OSEK I Labs on Tiva C using keil

---
+++ OSEK_I/KeilUvision/OsekInto_ResourceManagementProgrammingExercise/build_files/osekinto_resourcemanagementprogrammingexercise.d
@@ -0,0 +1,11 @@
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: OsekInto_ResourceManagementProgrammingExercise.c
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: ..\..\OsekLib\os.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: ..\..\oil\OsCfg.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: ..\..\OsekLib\OsTypes.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: SWTimer.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: gpio.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: sysctl.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: hw_memmap.h
+.\build_files\osekinto_resourcemanagementprogrammingexercise.o: tm4c123gh6pm.h
---
+++ OSEK_I/KeilUvision/OsekIntro_Example8/build_files/osekintro_example8.d
@@ -0,0 +1,6 @@
+.\build_files\osekintro_example8.o: OsekIntro_Example8.c
+.\build_files\osekintro_example8.o: ..\..\OsekLib\os.h
+.\build_files\osekintro_example8.o: ..\..\oil\OsCfg.h
+.\build_files\osekintro_example8.o: ..\..\OsekLib\OsTypes.h
+.\build_files\osekintro_example8.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\build_files\osekintro_example8.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
